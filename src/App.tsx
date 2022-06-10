b11383e8186b7e1eb12e3f657cd3c798101f38dd
royce-reed <rjreed1@gmail.com>
2022-06-09 20:44:50 -0500

    lint test

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,11 @@ import svgExample from './catching-catching-a-ball-person-svgrepo-com.svg';
 import { ClickCounter } from './ClickCounter';
 
 export const App = () => {
+  const name = 'John';
   return (
     <div>
       <h1>
-        Heller World - {process.env.NODE_ENV} {process.env.name}
+        Heller World - {name} {process.env.NODE_ENV} {process.env.name}
       </h1>
       <img src={image} alt="wood" />
       <img src={svgExample} alt="stick" />
