548408d8a048ba5969893175540d8955f95b9f7b
royce-reed <rjreed1@gmail.com>
2022-06-09 20:26:13 -0500

    setup eslint

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import './styles.css';
 import image from './woods2.jpg';
 import svgExample from './catching-catching-a-ball-person-svgrepo-com.svg';
 import { ClickCounter } from './ClickCounter';
+
 export const App = () => {
+  const name = 'John';
+  
   return (
     <div>
       <h1>Heller World - {process.env.NODE_ENV} {process.env.name}</h1>
