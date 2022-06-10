28b14a54de64d247aa3c938c3f2d1cbde704d9d6
royce-reed <rjreed1@gmail.com>
2022-06-09 19:23:33 -0500

    setup webpack for dev and prod

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import './styles.css';
+import image from './woods2.jpg';
+import svgExample from './catching-catching-a-ball-person-svgrepo-com.svg';
 export const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello World - {process.env.NODE_ENV} {process.env.name}</h1>
+      <img src={image} alt="wood" />
+      <img src={svgExample} alt="stick" />
     </div>
   );
 };
